2f99c9cd55213959567c9daf7f45b9e3eb0aa8b3
jdmeyer73 <jdmeyer@bgsu.edu>
2022-02-15 10:56:00 -0500

    Fix error in Topic 5; start Topic 6

---
+++ Topic06/eigtable.R
@@ -0,0 +1,25 @@
+eigtab <- function(pcaobj) {
+   eigtable <- data.frame(Component=character(),
+                          Eigenvalue=double(),
+                          Difference=double(),
+                          Proporation=double(),
+                          Cumulative=double())
+   len <- length(pcaobj$sdev)
+   for (i in 1:len) {
+      eigtable[i,1] <- paste("Comp",i)
+      eigtable[i,2] <- pcaobj$sdev[i]^2
+      if(i!=len) {
+         eigtable[i,3] <-  pcaobj$sdev[i]^2 - pcaobj$sdev[i+1]^2
+         } else {
+            eigtable[i,3] <- NA
+      }
+      eigtable[i,4] <- (pcaobj$sdev[i]^2)/len
+      if(i==1) {
+         eigtable[i,5] <- (pcaobj$sdev[i]^2)/len
+         } else {
+            eigtable[i,5] <- (pcaobj$sdev[i]^2)/len + eigtable[i-1,5]
+      }
+   }
+   eigtable
+}
+
